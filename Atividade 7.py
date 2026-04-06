eac6ffaa74d12a3c965b6a82e9717ded80193d60
Gabriela Falcão <gabifalcao55@gmail.com>
2026-04-06 19:27:33 -0300

    Delete Atividade 7.py

--- Atividade 7.py
+++
@@ -1,9 +0,0 @@
-#Leia um valor e verifique: Se é maior que 10 → Maior que 10; Caso contrário → Menor ou
-#igual a 10
-
-valor = int(input())
-
-if valor > 10:
-    print("Maior que 10")
-else:
-    print("Menor ou igual a 10")
