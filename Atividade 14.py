20dec25fd7c9429537e01d2d1091c213279c11e6
Gabriela Falcão <gabifalcao55@gmail.com>
2026-04-06 19:25:10 -0300

    Delete Atividade 14.py

--- Atividade 14.py
+++
@@ -1,9 +0,0 @@
-# Leia um valor e: Converta para inteiro; Se for múltiplo de 3 → “Múltiplo de 3”; Senão → “Não é
-#múltiplo”
-
-numero = int(input())
-
-if numero % 3 == 0:
-    print("Multiplo de 3")
-else:
-    print("Não é multiplo")
